af806532e643ecaf8f96f0183d4f0a9cbf15da9c
Steven Liekens <steven.liekens@gmail.com>
2013-03-10 17:57:10 +0100

    Switch Input/Output filenames around to match the containing types

--- API/Implementation/Input.vb
+++ API/Implementation/Input.vb
@@ -25,33 +25,68 @@
 Namespace API.Implementation
 
     ''' <summary>
-    ''' A helper class for comparing output parameters and creating new output parameters.
+    ''' A helper class for comparing input parameters and creating new input parameters.
     ''' </summary>
-    Friend NotInheritable Class Output : Inherits Parameter
+    Friend NotInheritable Class Input : Inherits Parameter
 
-        Private Shared ReadOnly _code As New Output("code")
-        Private Shared ReadOnly _remaining As New Output("remaining")
-        Private Shared ReadOnly _resettimer As New Output("resettimer")
+        Private Shared ReadOnly _apiKey As New Input("apikey")
+        Private Shared ReadOnly _application As New Input("application")
+        Private Shared ReadOnly _event As New Input("event")
+        Private Shared ReadOnly _description As New Input("description")
+        Private Shared ReadOnly _priority As New Input("priority")
+        Private Shared ReadOnly _url As New Input("url")
+        Private Shared ReadOnly _contentType As New Input("content-type")
+        Private Shared ReadOnly _developerkey As New Input("developerkey")
 
         Private Sub New(parameter As String)
             MyBase.New(parameter)
         End Sub
 
-        Public Shared ReadOnly Property Code As Output
+        Public Shared ReadOnly Property APIKey As Input
             Get
-                Return Output._code
+                Return Input._apiKey
             End Get
         End Property
 
-        Public Shared ReadOnly Property Remaining As Output
+        Public Shared ReadOnly Property Application As Input
             Get
-                Return Output._remaining
+                Return Input._application
             End Get
         End Property
 
-        Public Shared ReadOnly Property ResetTimer As Output
+        Public Shared ReadOnly Property [Event] As Input
             Get
-                Return Output._resettimer
+                Return Input._event
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property Description As Input
+            Get
+                Return Input._description
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property Priority As Input
+            Get
+                Return Input._priority
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property URL As Input
+            Get
+                Return Input._url
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property ContentType As Input
+            Get
+                Return Input._contentType
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property DeveloperKey As Input
+            Get
+                Return Input._developerkey
             End Get
         End Property
 
